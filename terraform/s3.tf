527a84fbf535561cc00f85420d902ca6f1a1a21f
choisungwook <kgg1959@naver.com>
2023-11-18 23:10:32 +0900

    KAN-26 Refactor resource block

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_public_access_block" "test" {
+resource "aws_s3_bucket_public_access_block" "this" {
   bucket                  = aws_s3_bucket.this.id
   block_public_acls       = true
   block_public_policy     = true
